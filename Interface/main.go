3873b5247ddd584f021371bf50ebae32c7d86fbf
ravi-9898 <you@example.com>
2024-05-29 16:53:07 -0400

    mod_changes

--- Interface/main.go
+++ Interface/main.go
@@ -2,44 +2,46 @@ package main
 
 import "fmt"
 
+// Define the 'person' struct
 type person struct {
 	Name string
 }
 
+// Define the 'secretAgent' struct, which embeds 'person'
 type secretAgent struct {
 	person
 	ltk bool
 }
 
+// Implement the 'speak()' method for 'person'
 func (p person) speak() {
 	fmt.Println("I Need help -", p.Name)
 }
 
+// Implement the 'speak()' method for 'secretAgent'
 func (sa secretAgent) speak() {
-	fmt.Println("Don't worry secret Agent -", sa.Name, "will be there soon")
+	fmt.Println("Don't worry, our secret agent", sa.Name, "will be there soon")
 }
 
+// Define the 'human' interface, which requires the 'speak()' method
 type human interface {
 	speak()
 }
 
-func command(h human) {
+// Define a function that takes a 'human' interface and calls the 'speak()' method
+func saysomething(h human) {
 	h.speak()
 }
 
 func main() {
-	p1 := person{
-		Name: "Sam",
-	}
-
+	// Create instances of 'person' and 'secretAgent'
+	p1 := person{Name: "John"}
 	sa := secretAgent{
-		person: person{
-			Name: "James",
-		},
-		ltk: true,
+		person: person{Name: "James Bond"},
+		ltk:    true,
 	}
 
-	command(p1)
-	command(sa)
-
+	// Call 'saysomething' with both instances
+	saysomething(p1)
+	saysomething(sa)
 }
