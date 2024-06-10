fc86781853a663a187f561a4c5b6ce6b9d2507d4
ravi-9898 <you@example.com>
2024-06-10 18:01:59 -0400

    marshal and unmarshal

---
+++ marshal/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	p1 := person{
+		FirstName: "James",
+		LastName:  "Bond",
+		Age:       32,
+	}
+
+	p2 := person{
+		FirstName: "Miss",
+		LastName:  "MonneyPenny",
+		Age:       27,
+	}
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	pp, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(pp))
+
+}
---
+++ unMarshal/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	FirstName string `json:"FirstName"`
+	LastName  string `json:"LastName"`
+	Age       int    `json:"Age"`
+}
+
+func main() {
+	jsonData := `[{"FirstName":"James","LastName":"Bond","Age":32},{"FirstName":"Miss","LastName":"MonneyPenny","Age":27}]`
+
+	bs := []byte(jsonData)
+
+	fmt.Printf("%T\n", jsonData)
+	fmt.Printf("%T\n", bs)
+
+	var people []person
+
+	err := json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("all the data", people)
+
+	for i, v := range people {
+		fmt.Println("\nperson number:", i)
+		fmt.Println(v.FirstName, v.LastName, v.Age)
+	}
+}
